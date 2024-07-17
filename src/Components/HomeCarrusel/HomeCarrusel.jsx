443ed63e276b8d5165a459b2510d9d3075a3bcda
Joaquin Pugliese <pugliesejoaquinsebastian214@gmail.com>
2024-07-17 13:06:04 -0300

    Cambios en las imagenes que fallaban

--- src/Components/HomeCarrusel/HomeCarrusel.jsx
+++ src/Components/HomeCarrusel/HomeCarrusel.jsx
@@ -6,8 +6,8 @@ function HomeCarrusel() {
             <div className="HomeCarruselContainer">
                 <ul>
                     <li><img src="https://d1qg2exw9ypjcp.cloudfront.net/assets/prod/100459/1200x0_p19s1r0nlf1psdshv8emoeei543.jpg" alt="Imagen Avon" /></li>
-                    <li><img src="src\assets\image\image_slider.jpg" alt="Imagen Avon" /></li>
-                    <li><img src="src\assets\image\image_slider2.jpg" alt="Imagen Avon" /></li>
+                    <li><img src="../assets/image/image_slider.jpg" alt="Imagen Avon" /></li>
+                    <li><img src="../assets/image/image_slider2.jpg" alt="Imagen Avon" /></li>
                 </ul>
             </div>
         </div>
